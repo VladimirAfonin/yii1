cda43f96565fb41fc45972296f34c37a731ebfc1
BruceBanner <afoninvladimir@yahoo.com>
2019-07-13 14:15:19 +0300

    - hidden some elements

--- protected/views/site/widget.php
+++ protected/views/site/widget.php
@@ -4,28 +4,28 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-<h3>test<?= var_dump($test = Yii::app()->user->isGuest) ?></h3>
+<!--<h1>Welcome to <i>--><?php //echo CHtml::encode(Yii::app()->name); ?><!--</i></h1>-->
+<h3><?= var_dump($test = Yii::app()->user->isGuest) ?></h3>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<!--<p>Congratulations! You have successfully created your Yii application.</p>
 
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
-<!--    <li>View file: <code>--><?php //echo __FILE__; ?><!--</code></li>-->
-<!--    <li>Layout file: <code>--><?php //echo $this->getLayoutFile('main'); ?><!--</code></li>-->
+<!--    <li>View file: <code>--><?php /*//echo __FILE__; */?><!--</code></li>-->
+<!--    <li>Layout file: <code>--><?php /*//echo $this->getLayoutFile('main'); */?><!--</code></li>
 </ul>
-
-<p>For more details on how to further develop this application, please read
+-->
+<!--<p>For more details on how to further develop this application, please read
     the <a href="http://www.yiiframework.com/doc/">documentation</a>.
     Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-    should you have any questions.</p>
+    should you have any questions.</p>-->
 
 <div class="people-info">
     <table class="table table-striped people-info">
         <thead>
         <tr class="tr-head">
             <td colspan="3">
-                <h3 class="head-txt">Personal</h3>
+                <p class="head-txt" style="margin: 0;">Personal</p>
             </td>
         </tr>
         </thead>
@@ -40,9 +40,9 @@ $this->pageTitle=Yii::app()->name;
                                     <span style="color: #c00;" class="icons fa fa-lock privacy_icon"></span>
                                     <span class="fa fa-edit"></span>
                                 </div>
-                                <div class="firstname-edit">
+                                <div class="firstname-edit hidden">
                                     <input class="form-control form-control-custom required" maxlength="25" placeholder="first name" type="text" value="Bala">
-                                    <span class="danger" id="f-name-error">First Name cannot be left blank</span>
+                                    <span class="danger hidden" id="f-name-error">First Name cannot be left blank</span>
                                 </div>
                             </div>
                         </div>
@@ -55,9 +55,9 @@ $this->pageTitle=Yii::app()->name;
                                             <span style="color: #c00;" class="icons fa fa-lock privacy_icon"></span>
                                             <span class="fa fa-edit"></span>
                                         </div>
-                                        <div class="middle-edit">
+                                        <div class="middle-edit hidden">
                                             <input class="form-control form-control-custom required" maxlength="25" placeholder="middle name" type="text" value="123">
-                                            <span class="danger" id="m-name-error">Middle Name cannot be left blank</span>
+                                            <span class="danger hidden" id="m-name-error">Middle Name cannot be left blank</span>
                                         </div>
                                     </div>
                                 </div>
@@ -68,9 +68,9 @@ $this->pageTitle=Yii::app()->name;
                                             <span style="color: #c00;" class="icons fa fa-lock privacy_icon"></span>
                                             <span class="fa fa-edit"></span>
                                         </div>
-                                        <div class="lastname-edit">
+                                        <div class="lastname-edit hidden">
                                             <input class="form-control form-control-custom required" maxlength="25" placeholder="last name" type="text" value="Bala kumar.">
-                                            <span class="danger" id="l-name-error">Last Name cannot be left blank</span>
+                                            <span class="danger hidden" id="l-name-error">Last Name cannot be left blank</span>
                                         </div>
                                     </div>
                                 </div>
@@ -79,15 +79,15 @@ $this->pageTitle=Yii::app()->name;
 
                         </div>
 <!--                        <div class="col-md-12">-->
-                            <span class="tooltip-custom tooltip-info">You can display your names differently to different users. #71</span>
+                            <span class="tooltip-custom tooltip-info hidden">You can display your names differently to different users. #71</span>
 <!--                        </div>-->
                     </div>
                 </td>
                 <td class="col-md-1">
                     <div class="icons icons-view">
                         <div class="action-icons">
-                            <span data-toggle="tooltip" title="Save" class="fa fa-save"></span>
-                            <span class="fa fa-close" title="Cancel #90"></span>
+                            <span data-toggle="tooltip" title="Save" class="fa fa-save hidden "></span>
+                            <span class="fa fa-close hidden " title="Cancel #90"></span>
                             <span class="fa fa-info-circle" title="Info"></span>
                         </div>
                     </div>
@@ -105,10 +105,10 @@ $this->pageTitle=Yii::app()->name;
                                     <span id="nickname-1">xyz</span>
                                     <span class="icons fa fa-lock" style="color: #3b5998;"></span>
                                 </div>
-                                <div class="nickname-edit">
+                                <div class="nickname-edit hidden">
                                     <input class="form-control form-control-custom required" maxlength="25" placeholder="nick name" type="text" value="xyz">
-                                    <!--                                <span class="danger" id="n-name-error">First Name cannot be left blank</span>-->
-                                    <span class="danger" id="n-name-error">Nick Name cannot be left blank</span>
+                                    <!--                                <span class="danger hidden" id="n-name-error">First Name cannot be left blank</span>-->
+                                    <span class="danger hidden" id="n-name-error">Nick Name cannot be left blank</span>
                                 </div>
                             </div>
 
@@ -119,9 +119,9 @@ $this->pageTitle=Yii::app()->name;
                     <div class="icons icons-view">
                         <div class="action-icons">
                             <span class="fa fa-edit" title="Edit"></span>
-                            <span data-toggle="tooltip" title="Save" class="fa fa-save"></span>
+                            <span data-toggle="tooltip" title="Save" class="fa fa-save hidden "></span>
                             <span data-toggle="tooltip" title="Delete" class="fa fa-trash"></span><br>
-                            <span class="fa fa-close" title="Cancel #90"></span>
+                            <span class="fa fa-close hidden " title="Cancel #90"></span>
                             <span class="fa fa-info-circle" title="Info"></span>
                         </div>
                     </div>
@@ -169,7 +169,7 @@ $this->pageTitle=Yii::app()->name;
                                 <span style="color: #3b5998;" class="icons fa fa-lock"></span>
                             </div>
                         </div>
-                        <div class="birth-edit">
+                        <div class="birth-edit hidden">
                             <div class="row">
                                 <div class="col-md-6 margin-select">
                                     <select class="form-control padding-select form-control-custom form-control-select" name="month" id="month">
@@ -233,10 +233,10 @@ $this->pageTitle=Yii::app()->name;
                                     </select>
                                 </div>
                             </div>
-                            <span class="danger" id="dob-error">Please choose the complete date as a your birthday</span>
-                            <span class="danger" id="dob-wrong">It look like you have entered the wrong info. Please make sure that you use real date of birth.</span>
-                            <span class="danger" id="dob-young">We are in process of developing this platform for younger people like you but are not ready currently. We regret inconvenience.</span>
-                            <span class="danger" id="dob-failed">Could not store date of birth. please try again.</span>
+                            <span class="danger hidden" id="dob-error">Please choose the complete date as a your birthday</span>
+                            <span class="danger hidden" id="dob-wrong">It look like you have entered the wrong info. Please make sure that you use real date of birth.</span>
+                            <span class="danger hidden" id="dob-young">We are in process of developing this platform for younger people like you but are not ready currently. We regret inconvenience.</span>
+                            <span class="danger hidden" id="dob-failed">Could not store date of birth. please try again.</span>
                         </div>
                     </div>
                     <span class="tooltip-info"></span>
@@ -246,8 +246,8 @@ $this->pageTitle=Yii::app()->name;
                 <div class="icons icons-view">
                     <div class="action-icons">
                         <span class="fa fa-edit" title="Edit"></span>
-                        <span data-toggle="tooltip" title="Save" class="fa fa-save"></span>
-                        <span class="fa fa-close" title="Cancel #90"></span>
+                        <span data-toggle="tooltip" title="Save" class="fa fa-save hidden "></span>
+                        <span class="fa fa-close hidden " title="Cancel #90"></span>
                         <span class="fa fa-info-circle" title="Info"></span>
                     </div>
                 </div>
@@ -264,7 +264,7 @@ $this->pageTitle=Yii::app()->name;
                             <span id="aboutme-1">This is all about how great I am at this site.....</span>
                             <span class="icons fa fa-lock privacy_icon"></span>
                         </div>
-                        <div class="aboutme-edit">
+                        <div class="aboutme-edit hidden">
                             <textarea class="form-control form-control-textarea required"
                                       maxlength="200" name="aboutme" id="aboutme-edit"
                                       placeholder="Comments max 200 characters"
@@ -278,7 +278,7 @@ $this->pageTitle=Yii::app()->name;
                         </div>
                     </div>
                 </div>
-                <span class="tooltip-custom tooltip-info">
+                <span class="tooltip-custom tooltip-info hidden">
                     Your brief Introduction to MyLokal Community #79
                 </span>
             </td>
@@ -286,8 +286,8 @@ $this->pageTitle=Yii::app()->name;
                 <div class="icons icons-view">
                     <div class="action-icons">
                         <span class="fa fa-edit" title="Edit"></span>
-                        <span data-toggle="tooltip" title="Save" class="fa fa-save"></span>
-                        <span class="fa fa-close" title="Cancel #90"></span>
+                        <span data-toggle="tooltip" title="Save" class="fa fa-save hidden "></span>
+                        <span class="fa fa-close hidden " title="Cancel #90"></span>
                         <span class="fa fa-info-circle" title="Info"></span>
                     </div>
                 </div>
@@ -304,7 +304,7 @@ $this->pageTitle=Yii::app()->name;
                             <span id="relation-1">Divorced</span>
                             <span class="icons fa fa-lock" style="color: #3b5998;"></span>
                         </div>
-                        <div class="relation-edit">
+                        <div class="relation-edit hidden">
                             <select id="relationStatus" class="form-control form-control-select filter-select" name="relationStatus">
                                 <option value="316">---</option>
                                 <option value="317">Single</option>
@@ -319,11 +319,11 @@ $this->pageTitle=Yii::app()->name;
                                 <option value="326" selected="">Divorced</option>
                                 <option value="327">Widowed</option>
                             </select>
-                            <span class="danger" id="relation-error">Please select your relationship status</span>
+                            <span class="danger hidden" id="relation-error">Please select your relationship status</span>
                         </div>
                     </div>
                 </div>
-                <span class="tooltip-custom tooltip-info">
+                <span class="tooltip-custom tooltip-info hidden">
                     Your brief Introduction to MyLokal Community #79
                 </span>
             </td>
@@ -332,8 +332,8 @@ $this->pageTitle=Yii::app()->name;
                     <div class="action-icons">
                         <span class="fa fa-edit" title="Edit"></span>
                         <span data-toggle="tooltip" title="Delete" class="fa fa-trash"></span><br>
-                        <span data-toggle="tooltip" title="Save" class="fa fa-save"></span>
-                        <span class="fa fa-close" title="Cancel #90"></span>
+                        <span data-toggle="tooltip" title="Save" class="fa fa-save hidden "></span>
+                        <span class="fa fa-close hidden " title="Cancel #90"></span>
                         <span class="fa fa-info-circle" title="Info"></span>
                     </div>
                 </div>
@@ -349,14 +349,14 @@ $this->pageTitle=Yii::app()->name;
                         <div class="keywords-view">
                             <span id="keywords">Keywords</span>
                         </div>
-                        <div class="keywords-edit">
+                        <div class="keywords-edit hidden">
                             <div>
                                 <input type="text" class="form-control form-control-custom" placeholder="Click here to add new tag" maxlength="25">
-                                <span class="danger"></span>
+                                <span class="danger hidden"></span>
                             </div>
                         </div>
                     </div>
-                    <span class="tooltip-custom tooltip-info">
+                    <span class="tooltip-custom tooltip-info hidden">
                         Your brief Introduction to MyLokal Community #79
                     </span>
                 </div>
@@ -365,7 +365,7 @@ $this->pageTitle=Yii::app()->name;
                 <div class="icons icons-view">
                     <div class="action-icons">
                         <span class="fa fa-edit" title="Edit"></span>
-                        <span class="fa fa-close" title="Cancel #90"></span>
+                        <span class="fa fa-close hidden " title="Cancel #90"></span>
                         <span class="fa fa-info-circle" title="Info"></span>
                     </div>
                 </div>

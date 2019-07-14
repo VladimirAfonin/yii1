2a7a434b2fab78fa54f058403ef6fdf15a9d80f3
BruceBanner <afoninvladimir@yahoo.com>
2019-07-14 12:35:16 +0300

    - remove comment line from 'common.js'

--- js/common.js
+++ js/common.js
@@ -345,22 +345,3 @@ $('#relation-trash').on('click', function(e){
         },
     });
 });
-
-
-/*$(document).on('mouseover', 'tr', function(e) {
-    var _this = $(this);
-    // var el = _this.find('.icons-view'); // .replaceClass('hidden', 'visible');
-    // el.replaceClass('hidden', 'visible')
-    // $('.fa-info-circle').removeClass('hidden').addClass('visible');
-}).on('mouseout', 'tr', function(e) {
-    $('.fa-info-circle').removeClass('visible').addClass('hidden');
-});*/
-/*
-
-$('tr').mouseover(function(){
-    // $(this).find('td:last')
-    // $('.fa-info-circle').removeClass('hidden').addClass('visible');
-}).mouseout(function(){
-    $('.fa-info-circle').removeClass('visible').addClass('hidden');
-});
-*/
